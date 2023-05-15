142cc49c184fdbbd299a6276fe7fc88bfa2dd0fb
BThomas <bran_thom@yahoo.com>
2023-05-15 12:58:26 -0500

    temperature toggle with api call

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+// const toggleButton = document.querySelector("#toggle-button");
+// toggleButton.addEventListener("click", converttemp);
 console.log("im connected")
 function hide(element) {
     element = document.querySelector("#cookie-holder");
@@ -8,8 +9,8 @@ function hide(element) {
 // "lat":36.1622767,"lon":-86.7742984,"
 // "https://api.openweathermap.org/data/2.5/forecast?lat=36.1622767&lon=-86.7742984&appid=0f6ddb4f4ad9e55fb15c16fe116e0087"
 
-async function getweather() {
-    var response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Nashville, US&cnt=5&appid=0f6ddb4f4ad9e55fb15c16fe116e0087&units=imperial");
+async function getweather(units) {
+    var response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Nashville, US&appid=0f6ddb4f4ad9e55fb15c16fe116e0087&units=${units}`);
     var weatherData = await response.json();
 
     console.log(weatherData);
@@ -23,16 +24,21 @@ async function getweather() {
     weather_high.innerText = Math.trunc(high_temp);
     console.log(weather_pic);
     
-    return weather_pic;
+    return [weather_pic,low_temp,high_temp];
 }
 
 async function show_weather() {
-    const weather_pic = await getweather();
     var weather_pic_element = document.getElementById("img_element");
     var forecast_description = document.getElementById("forecast")
+    const currentDate = new Date();
+    const dateElement = document.getElementById("date");
+    const units = document.getElementById("temperature").value;
+    const [weather_pic,low_temp, high_temp] = await getweather(units);
+    dateElement.innerText = currentDate.toDateString();
     
     if (weather_pic === "Clear" || weather_pic === "Sunny") {
         weather_pic_element.src = "assets/some_sun.png";
+        forecast_description.innerText = "Some Sun"
         
     } else {
         weather_pic_element.src = "assets/some_clouds.png";
@@ -45,3 +51,16 @@ async function show_weather() {
 }
 
 show_weather();
+
+async function converttemp(){
+    
+    var units = document.getElementById("temperature").value
+    if (units === "imperial"){
+        units = "metric"
+    }
+    else{
+        units = "imperial"
+    }
+    await show_weather(units)
+}
+document.getElementById("temperature").addEventListener("change", converttemp);
